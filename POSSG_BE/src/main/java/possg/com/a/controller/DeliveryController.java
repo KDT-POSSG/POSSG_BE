da6ac74f857ad00c8eed835c416e210236e7e98d
rudxo <bhj691226@naver.com>
2023-09-12 10:29:20 +0900

    [Add] deliver func

--- POSSG_BE/src/main/java/possg/com/a/controller/DeliveryController.java
+++ POSSG_BE/src/main/java/possg/com/a/controller/DeliveryController.java
@@ -1,6 +1,8 @@
 package possg.com.a.controller;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -18,8 +20,11 @@ import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import possg.com.a.dto.ConvenienceDto;
 import possg.com.a.dto.DeliveryDto;
+import possg.com.a.dto.DeliveryListDto;
 import possg.com.a.dto.DeliveryParam;
+import possg.com.a.dto.ProductDto;
 import possg.com.a.service.DeliveryService;
+import possg.com.a.service.ProductService;
 import possg.com.a.util.SecurityConfig;
 
 @RestController
@@ -31,11 +36,14 @@ public class DeliveryController {
 	@Autowired
 	SecurityConfig securityConfig;
 	
+	@Autowired
+	ProductService productService;
+	
 	
 	// 점주 배달 추가
 	@PostMapping("convAddDelivery")
 	public String convAddDelivery(ConvenienceDto dto, @CookieValue("accessToken") String accessToken) {
-		System.out.println("DeliveryController addcustomer " + new Date());
+		System.out.println("DeliveryController convAddDelivery " + new Date());
 		
 		//토큰 파싱
 				accessToken = accessToken.replace("Bearer ", "");
@@ -57,7 +65,75 @@ public class DeliveryController {
 		}		
 		return "NO";
 	}
+	
+	// 배달 장바구니에 추가
+	@PostMapping("callAddDelivery")
+	public String callAddDelivery(DeliveryDto dto) {
+		System.out.println("DeliveryController callAddDelivery " + new Date());
+		
+		// 갯수에 따라 가격 맞춰서 넣기			
+		ProductDto product = new ProductDto();
+		
+		product.setProductName(dto.getProductName());
+		
+		List<ProductDto> prodto = productService.findProductName(product);
+		
+		dto.setPrice(dto.getQuantity() * prodto.get(0).getPrice());
+		
+		// 성공 실패
+		int count = service.callAddDelivery(dto);						
+		if(count != 0) {
+			return "YES";
+		}
+		return "NO";
+	}
+	
+	// 배달 장바구니 목록 가져오기
+	
+	
+	
+	// 배달 주문
+	@PostMapping("insertDeliveryList")
+	public String insertDeliveryList(DeliveryListDto dto) {
+		System.out.println("DeliveryController callAddDelivery " + new Date());
+			
+		DeliveryDto delivery = new DeliveryDto();
+		
+		delivery.setRef(dto.getDelRef());
+		
+		delivery.setUserId(dto.getUserId());
 
+		List<DeliveryDto> deli = service.selectDelivery(delivery);
+		int price = 0;
+		int quantity = 0;
+		for(DeliveryDto forDto :deli) {
+			price = price + forDto.getPrice();
+			quantity = quantity + forDto.getQuantity();		
+		}		
+		
+		 // 현재 날짜와 시간 얻기
+        LocalDateTime currentDateTime = LocalDateTime.now();        
+        // 날짜와 시간을 원하는 형식으로 포맷팅
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        
+        formattedDateTime = formattedDateTime.replaceAll("[^\\d]", "");
+        
+        dto.setDelTotalPrice(price);
+		dto.setDelTotalNumber(quantity);
+		dto.setDelRef(formattedDateTime);
+		
+		int count = service.insertDeliveryList(dto);
+		
+		delivery.setRef(formattedDateTime);
+		service.updateDelivery(delivery);
+
+		if(count != 0) {
+			return "YES";
+		}
+		return "NO";
+	}
+	
 
 	// 배달목록 누르면 detail 페이지 상세보기
 	@GetMapping("allDeliveryList")
@@ -70,7 +146,7 @@ public class DeliveryController {
 		return delivery;	
 	}
 	
-	
+
 
 /*
 	@GetMapping("allDeliveryList")
