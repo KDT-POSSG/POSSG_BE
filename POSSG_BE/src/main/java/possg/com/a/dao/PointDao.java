b5718924db2097a6fa56069fad72f35cb45d1e2a
mingyu choi <52327569+mango9236@users.noreply.github.com>
2023-10-05 17:40:34 +0900

    [Add] point func and fix attendance bug

--- POSSG_BE/src/main/java/possg/com/a/dao/PointDao.java
+++ POSSG_BE/src/main/java/possg/com/a/dao/PointDao.java
@@ -3,8 +3,14 @@ package possg.com.a.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import possg.com.a.dto.PointParam;
+
 @Mapper
 @Repository
 public interface PointDao {
-
+	int newPoint(String phoneNumber);
+	int checkPoint(String phoneNumber);
+	int addPoint(PointParam param);
+	int searchPoint(String phoneNumber);
+	int usePoint(PointParam param);
 }
--- POSSG_BE/src/main/java/possg/com/a/service/PointService.java
+++ POSSG_BE/src/main/java/possg/com/a/service/PointService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import possg.com.a.dao.PointDao;
+import possg.com.a.dto.PointParam;
 
 @Service
 @Transactional
@@ -12,4 +13,24 @@ public class PointService {
 	
 	@Autowired
 	PointDao dao;
+	
+	public int newPoint(String phoneNumber) {
+		return dao.newPoint(phoneNumber);
+	};
+	
+	public int checkPoint(String phoneNumber) {
+		return dao.checkPoint(phoneNumber);
+	};
+	
+	public int addPoint(PointParam param) {
+		return dao.addPoint(param);
+	};
+	
+	public int searchPoint(String phoneNumber) {
+		return dao.searchPoint(phoneNumber);
+	};
+	
+	public int usePoint(PointParam param) {
+		return dao.usePoint(param);
+	};
 }
