4b14fea6a2d6d7fdb776b3b7a3aa54c6e959440e
mingyu choi <52327569+mango9236@users.noreply.github.com>
2023-10-11 16:36:23 +0900

    [Fix] change KST time

--- POSSG_BE/src/main/java/possg/com/a/controller/SSEController.java
+++ POSSG_BE/src/main/java/possg/com/a/controller/SSEController.java
@@ -23,5 +23,33 @@ public class SSEController {
     public Flux<List<ProductDto>> getNotifications() {
     	return service.getExpiredProductsStream();
     }
+    
+    /* 
+     * Emitter를 이용한 방법 --> 차후에 쓸만하면 써보자
+     * 
+    public SseEmitter getNotifications() {
+        SseEmitter emitter = new SseEmitter(60_000L);  // 1분 동안 연결을 유지
+        
+        Flux<List<ProductDto>> flux = service.getExpiredProductsStream();
+
+        flux.subscribe(
+                products -> {
+                    try {
+                        emitter.send(products);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> emitter.completeWithError(error),  // 에러 발생 시
+                emitter::complete  // 모든 데이터 전송 완료 시
+        );
+
+        // 클라이언트 연결 종료 시, SSEEmitter를 제대로 종료합니다.
+        emitter.onTimeout(() -> emitter.complete());
+        emitter.onError((e) -> emitter.complete());
+
+        return emitter;
+    }
+    */
 }
 
